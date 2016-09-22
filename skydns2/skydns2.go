6e0bafcb0f9c030f6e8c64bc99a512b37a1afdc1
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2016-09-22 14:03:06 +0200

    Add TLS etcd2 support to skydns

--- skydns2/skydns2.go
+++ skydns2/skydns2.go
@@ -19,14 +19,20 @@ type Factory struct{}
 func (f *Factory) New(uri *url.URL) bridge.RegistryAdapter {
 	urls := make([]string, 0)
 	if uri.Host != "" {
-		urls = append(urls, "http://"+uri.Host)
+		urls = append(urls, "https://"+uri.Host)
 	}
 
 	if len(uri.Path) < 2 {
 		log.Fatal("skydns2: dns domain required e.g.: skydns2://<host>/<domain>")
 	}
 
-	return &Skydns2Adapter{client: etcd.NewClient(urls), path: domainPath(uri.Path[1:])}
+	tlskey := os.Getenv("ETCD_TLSKEY")
+	tlspem := os.Getenv("ETCD_TLSPEM")
+	cacert := os.Getenv("ETCD_CACERT")
+
+	client, err := etcd.NewTLSClient(urls, tlspem, tlskey, cacert)
+
+	return &Skydns2Adapter{client: client, path: domainPath(uri.Path[1:])}
 }
 
 type Skydns2Adapter struct {
